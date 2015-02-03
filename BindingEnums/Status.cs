d5b5c392d12dfe10259672d509aad70762601fcd
Brian Lagunas <brianlagunas@users.noreply.github.com>
2015-02-03 11:57:29 -0700

    initial add to source control

---
+++ BindingEnums/Status.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace BindingEnums
+{
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum Status
+    {
+        [Description("This is horrible")]
+        Horrible,
+        [Description("This is bad")]
+        Bad,
+        [Description("This is so so")]
+        SoSo,
+        [Description("This is good")]
+        Good,
+        [Description("This is better")]
+        Better,
+        [Description("This is best")]
+        Best
+    }
+}
